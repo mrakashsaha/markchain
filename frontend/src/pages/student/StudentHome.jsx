0ccf53f27c62960bccb0b657df4774fd74a29d73
hossainshofiq <ah731710@gmail.com>
2025-08-17 17:54:28 +0600

    resolve conflicts

--- frontend/src/pages/student/StudentHome.jsx
+++ frontend/src/pages/student/StudentHome.jsx
@@ -2,27 +2,26 @@ import React from 'react';
 
 const StudentHome = () => {
 
-    const handleViewGrades = () => {
-        // Logic to view grades
-        console.log('Viewing grades...');
-    };
+    // const handleViewGrades = () => {
+    //     // Logic to view grades
+    //     console.log('Viewing grades...');
+    // };
 
-    const handleLogout = () => {
-        // Logic to logout
-        console.log('Logging out...');
-    };
+    // const handleLogout = () => {
+    //     // Logic to logout
+    //     console.log('Logging out...');
+    // };
 
     return (
         <div>
             {/* <h1>Student Dashboard</h1> */}
             <p>Welcome to your dashboard!</p>
-            <div className='my-6 p-3 border'>
-                {/* add all subjects marks */}
+            {/* <div className='my-6 p-3 border'>
                 <button onClick={handleViewGrades} className='btn btn-wide btn-neutral'>View Marks</button>
             </div>
             <div className='flex justify-end'>
                 <button onClick={handleLogout} className='btn btn-wide btn-neutral'>Logout</button>
-            </div>
+            </div> */}
         </div>
     );
 };
